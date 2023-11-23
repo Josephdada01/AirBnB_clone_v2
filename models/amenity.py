b256e49ac5dccc1a803cad24a250926d00007c98
Josephdada01 <ridwandada500@gmail.com>
2023-11-23 11:19:32 +0100

    task 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from models.place import place_amenity
+
+
+"""
+place_amenity = Table(
+    'place_amenity', Base.metadata,
+    Column('place_id', Integer, ForeignKey('places.id')),
+    Column('amenity_id', Integer, ForeignKey('amenities.id'))
+)
+"""
 
 
 class Amenity(BaseModel):
     """Amenity class"""
-    name = ""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity,
+                                   back_populates="amenity_places")
+    id = Column(Integer, primary_key=True)
+
--- models/place.py
+++ models/place.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship, sessionmaker
 from models.review import Review
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import MetaData
+from models.amenity import Amenity
 
+
+
+Base = declarative_base()
+"""
+place_amenity = Table(
+    'place_amenity', Base.metadata,
+    Column('place_id', String(60), ForeignKey('places.id'),
+           primary_key=True, nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'),
+           primary_key=True, nullable=False)
+"""
 
 class Place(BaseModel):
     """ A place to stay """
     __tablename__ = 'places'
+    id = Column(Integer, primary_key=True)
     city_id = Column(String(60), nullable=False)
     user_id = Column(String(60), nullable=False)
     name = Column(String(128), nullable=False)
@@ -22,6 +37,17 @@ class Place(BaseModel):
     amenity_ids = []
     reviews = relationship("Review", backref="place",
                            cascade="all, delete-orphan")
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                                 Column('amenity_id', String(60),
+                                        ForeignKey('amenities.id'),
+                                        primary_key=True, nullable=False)
+                                        )
+    
+    amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False, backref="places")
     
     @property
     def reviews(self):
@@ -37,3 +63,26 @@ class Place(BaseModel):
             review for review in d_review.values() if review.place_id == self.id
         ]
         return matching_reviews
+    
+
+    @property
+    def amenities(self):
+        """
+        Getter attribute amenities that returns the list of Amenity
+        instances based on
+        the attribute amenity_ids that contains all
+        Amenity.id linked to the Place
+        """
+        if self.amenity_ids:
+            return [session.query(Amenity).get(int(amenity_id)) for
+                    amenity_id in self.amenity_ids.split(",")]
+        else:
+            return []
+        
+    @amenities.setter
+    def amenities(self, amenity):
+        if isinstance(amenity, Amenity):
+            if self.amenity_ids:
+                self.amenity_ids += f",{amenity.id}"
+            else:
+                self.amenity_ids = str(amenity.id)
