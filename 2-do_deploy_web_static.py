35a065390c782e32d98bcae7be4a0d9254e2f584
Josephdada01 <dadaridwan@500@gmail.com>
2024-01-12 06:32:54 -0800

    made some changes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Write a Fabric script (based on the file 1-pack_web_static.py) that
-distributes an archive to your web servers, using the function do_deploy
+distributes an archive to my web servers, using the function do_deploy
 """
 from fabric.api import local, run, env, put
 from fabric.contrib.files import exists
