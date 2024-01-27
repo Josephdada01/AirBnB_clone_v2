a88e559287a4b70645b547cd01c68a35fd52fcb0
Josephdada01 <ridwandada500@gmail.com>
2024-01-27 11:36:20 +0000

    task completed

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" a script that starts a Flask web application:"""
+from flask import Flask, escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display_hello():
+    """ it display hbnb """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """ it will display hbnb """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    """
+    display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(escape(text))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_text(text="is cool"):
+	"""
+	display “Python ”, followed by the value of the text variable
+	"""
+	text = text.replace('_', ' ')
+	return 'Python {}'.format(escape(text))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
