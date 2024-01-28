9f7ccfdfab5dac9f70ad3eba1cd2bafaa1ce5731
Josephdada01 <ridwandada500@gmail.com>
2024-01-28 21:36:23 +0100

    fixed pycodestyle

--- models/state.py
+++ models/state.py
@@ -21,9 +21,11 @@ class State(BaseModel, Base):
     else:
         @property
         def cities(self):
-            """Getter method to return the list of City objects linked to the current State"""
+            """Getter method to return the list of City
+            objects linked to the current State"""
             city_list = []
             for city_obj in models.storage.all(City).values():
                 if city_obj.state_id == self.id:
                     city_list.append(city_obj)
             return city_list
+        
