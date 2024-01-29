a776ea60b651b1b261553044e80dae1529845ce4
Josephdada01 <ridwandada500@gmail.com>
2024-01-29 10:31:04 +0100

    task to list states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """a function that render lists of states"""
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    return render_template('states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """
+    to perform cleanup or resource release operations
+    after each request has been processed
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
